3da6caa97d2f5b06de0dc9c613d314f8eab6262f
ypm1999 <yang1999422@163.com>
2019-03-31 10:43:08 +0800

    try_build_ir

--- src/src/com/mxcomplier/Ir/ProgramIR.java
+++ src/src/com/mxcomplier/Ir/ProgramIR.java
@@ -1,4 +1,13 @@
 package com.mxcomplier.Ir;
 
+import com.mxcomplier.Ir.Operands.StaticDataIR;
+
+import java.util.List;
+
 public class ProgramIR {
+    private List<BasicBlockIR> BBList;
+    private List<FuncIR> funcs;
+    private FuncIR mainFunc;
+    private List<StaticDataIR> staticData;
+
 }
